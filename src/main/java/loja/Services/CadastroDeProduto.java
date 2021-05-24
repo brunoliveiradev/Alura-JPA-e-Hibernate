46799efff49b9668614f723d18b2f489c3f05472
BRUNOOLIVEIRA13 <bruno.henrique0liveir4@gmail.com>
2021-05-24 18:51:36 -0300

    Entity Mapping and Relationship mapping.

--- src/main/java/loja/Services/CadastroDeProduto.java
+++ src/main/java/loja/Services/CadastroDeProduto.java
@@ -1,32 +1,32 @@
 package loja.Services;
 
+import loja.DAO.CategoriaDAO;
 import loja.DAO.ProdutoDAO;
+import loja.model.Categoria;
 import loja.model.Produto;
 import loja.util.JPAUtil;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 import java.math.BigDecimal;
 
 public class CadastroDeProduto {
 
     public static void main(String[] args) {
-        Produto celular = new Produto();
-        celular.setNome("Xiaomi Redmi");
-        celular.setDescricao("Smartphone");
-        celular.setPreco(new BigDecimal("800"));
+        Categoria celulares = new Categoria("CELULAR");
+        Produto celular = new Produto("Xiaomi Redmi", "Smartphone", new BigDecimal("800"), celulares);
 
         // Utiliza uma classe de suporte para pegar a conexão do Entity Manager
         EntityManager em = JPAUtil.getEntityManager();
-        ProdutoDAO dao = new ProdutoDAO(em);
+        ProdutoDAO produtoDAO = new ProdutoDAO(em);
+        CategoriaDAO categoriaDAO = new CategoriaDAO(em);
 
         //Com os frameworks, as transações, o commit e o close são feito por eles
 
         // Iniciar uma transação
         em.getTransaction().begin();
         //Fazer insert na tabela
-        dao.cadastrar(celular);
+        categoriaDAO.cadastrar(celulares); // cadastro da categoria
+        produtoDAO.cadastrar(celular);
         // Commitar a transação
         em.getTransaction().commit();
         // Fechar a transação
--- src/main/java/loja/model/Categoria.java
+++ src/main/java/loja/model/Categoria.java
@@ -1,4 +1,29 @@
 package loja.model;
 
-public enum Categoria {
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Categoria() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
